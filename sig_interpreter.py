ce3de4577650ae6c7b89ae61a4426d187f9890ed
Tomer Saban <sab30226@gmail.com>
2021-12-26 00:07:32 +0200

    Small improvements

--- sig_interpreter.py
+++ sig_interpreter.py
@@ -3,40 +3,51 @@
 import sys
 import subprocess
 from subprocess import run
-from pprint import pprint
-from typing import Tuple, Dict
+from typing import Tuple, Dict, Iterable
+
 
 def print_usage() -> None:
     print(
-    f"""
+        f"""
     Usage: {sys.argv[0]} <pid>
     """)
 
-def interpet_signal(hexdecimal_string: str) -> Tuple[str, ...]:
+
+def prepare_bits(hexdecimal_string: str) -> Iterable[Tuple[int, str]]:
+    decimal_string = int(hexdecimal_string, 16)  # Convert hexdecimal to decimal
+    binary_string = list(bin(decimal_string))  # Convert to binary list
+    binary_string = binary_string[len('0b'):]  # Removing 0b prefix
+    return enumerate(reversed(binary_string), 1)
+
+
+def interpret_signal(hexdecimal_string: str) -> Tuple[str, ...]:
     output = []
-    for place, bit in enumerate(reversed(list(bin(int(hexdecimal_string, 16)))[2:]), 1):
-        if bit == "1":
-            cmd = f'kill -l {place}'
-            completed_process = run(cmd, text=True, stdout=subprocess.PIPE, shell=True)
-            if completed_process.returncode == 0:
-                output.append(completed_process.stdout[:-1])
-            else:
-                print(completed_process.returncode)
+    for place, bit in prepare_bits(hexdecimal_string):
+        if bit != "1":
+            continue
+        cmd = f'kill -l {place}'
+        completed_process = run(cmd, text=True, stdout=subprocess.PIPE, shell=True)
+        if completed_process.returncode == 0:
+            output.append(completed_process.stdout.strip('\n'))
+        else:
+            print('error', f'{completed_process}')
     return tuple(output)
+
 
 def main() -> None:
     if len(sys.argv) != 2:
         print_usage()
         sys.exit(1)
-    pid: int = sys.argv[1]
+    pid: int = int(sys.argv[1])
     signals: Dict[str, Tuple[str, ...]] = {}
     with open(f'/proc/{pid}/status') as f:
         for line in f.readlines():
-            if 'Sig' in line and not 'SigQ' in line:
+            if 'Sig' in line and 'SigQ' not in line:
                 key, hexdecimal_string = line[:-1].split(':\t')
-                signals[key] = interpet_signal(hexdecimal_string)
+                signals[key] = interpret_signal(hexdecimal_string)
     for key, value in signals.items():
         print(f'Signal [{key}] -> {value}')
+
 
 if __name__ == "__main__":
     main()
